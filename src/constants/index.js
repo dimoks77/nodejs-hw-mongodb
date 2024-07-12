69132e0a1e9b8b8a7d77d2b3a22ff151e8fe170f
Dmytro Kasko <dimoks77@gmail.com>
2024-07-12 14:03:45 +0200

    auth.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,15 +1,18 @@
 export const ENV_VARS = {
-  PORT: 'PORT',
+  PORT: "PORT",
 };
 
 export const MONGO_VARS = {
-  MONGODB_USER: 'MONGODB_USER',
-  MONGODB_PASSWORD: 'MONGODB_PASSWORD',
-  MONGODB_URL: 'MONGODB_URL',
-  MONGODB_DB: 'MONGODB_DB',
+  MONGODB_USER: "MONGODB_USER",
+  MONGODB_PASSWORD: "MONGODB_PASSWORD",
+  MONGODB_URL: "MONGODB_URL",
+  MONGODB_DB: "MONGODB_DB",
 };
 
 export const SORT_ORDER = {
-  ASC: 'asc',
-  DESC: 'desc',
+  ASC: "asc",
+  DESC: "desc",
 };
+
+export const FIFTEEN_MINUTES = 15 * 60 * 1000;
+export const ONE_DAY = 24 * 60 * 60 * 1000;
